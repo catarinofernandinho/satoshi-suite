ce796b11d9367656ea35d41fea63bd5bc8330e0a
catarino <catarinafernandes.ct@gmail.com>
2025-08-03 13:49:48 -0300

    Update AddFutureModal.tsx

--- src/components/futures/AddFutureModal.tsx
+++ src/components/futures/AddFutureModal.tsx
@@ -75,7 +75,9 @@ export default function AddFutureModal({
     }
   };
   return <Dialog open={modalOpen} onOpenChange={setModalOpen}>
-      
+      {!isOpen && <DialogTrigger asChild>
+          
+        </DialogTrigger>}
       <DialogContent className="sm:max-w-[600px]">
         <DialogHeader>
           <DialogTitle>Adicionar Nova Ordem</DialogTitle>
